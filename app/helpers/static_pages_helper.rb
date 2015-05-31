753e3757ab170504b474016a28a1ff6f843a10ca
Zhou You <yollyhideout@gmail.com>
2015-05-31 17:14:58 +0800

    Finish layout and routes

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,12 @@
 module StaticPagesHelper
+
+	#return a title for all pages
+	def full_title(page_title = '')
+		base_title = "Footprint"
+		if page_title.empty?
+			return base_title
+		else
+			return page_title + " | " + base_title
+		end
+	end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,5 +19,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | Footprint"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Footprint"
+  end
+
 
 end
