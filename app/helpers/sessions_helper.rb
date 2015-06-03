ed2c24f62c4edad33b2b3d218db73a232acc705e
Zhou You <yollyhideout@gmail.com>
2015-06-03 17:17:12 +0800

    Finish remember me

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,18 +12,18 @@ module SessionsHelper
 		cookies.permanent[:remember_token] = user.remember_token
 	end
 
-	#return the current user object or nil if ther user does not exist
-	def current_user
-  		if (user_id = session[:user_id])
-    		@current_user ||= User.find_by(id: user_id)
-  		elsif (user_id = cookies.signed[:user_id])
-    		user = User.find_by(id: user_id)
-    		if user && user.authenticated?(cookies[:remember_token])
-      		log_in user
-      		@current_user = user
-    		end
-  		end
-	end
+    # Returns the user corresponding to the remember token cookie.
+    def current_user
+      if (user_id = session[:user_id])
+        @current_user ||= User.find_by(id: user_id)
+      elsif (user_id = cookies.signed[:user_id])
+        user = User.find_by(id: user_id)
+        if user && user.authenticated?(cookies[:remember_token])
+          log_in user
+          @current_user = user
+        end
+      end
+    end
 
 	#return true if the user is logged in, false otherwise
 	def logged_in?
