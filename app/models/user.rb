41251a5c19b5a1214ca0110cf444f49d57140519
Zhou You <yollyhideout@gmail.com>
2015-06-05 17:46:30 +0800

    Account activations resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token
+	before_save :downcase_email
+	before_save :create_activation_digest
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 	before_save { self.email = email.downcase }
@@ -40,4 +42,17 @@ class User < ActiveRecord::Base
     def forget
     	update_attribute(:remember_token, nil)
     end
+
+    private
+
+      # Converts email to all lower-case.
+      def downcase_email
+      	self.email = email.downcase
+      end
+
+      # Creates and assigns the activation token and digest
+      def create_activation_digest
+      	self.activation_token = User.new_token
+      	self.activation_digest = User.digest(activation_token)
+      end
 end
