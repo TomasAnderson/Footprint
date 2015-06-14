74a0c295615bf399461bc62707914065f24c0390
Zhou You <yollyhideout@gmail.com>
2015-06-14 15:22:51 +0800

    Finish the relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,45 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
-  has_many :active_relationships, class_name: "Relationship",
-                                  foreign_key: "follower_id",
-                                    dependent: :destroy
-	attr_accessor :remember_token, :activation_token, :reset_token
-	before_save :downcase_email
-	before_save :create_activation_digest
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  has_many :active_relationships, 
+  class_name: "Relationship",
+  foreign_key: "follower_id",
+  dependent: :destroy 
+  
+  has_many :passive_relationships,
+  class_name: "Relationship",
+  foreign_key: "followed_id",
+  dependent: :destroy
 
-	before_save { self.email = email.downcase }
-	validates :name, presence:  true, length:{maximum: 50}
-	validates :email, presence: true, length:{maximum: 255},
-				format:{ with: VALID_EMAIL_REGEX}, 
-				uniqueness: {case_sensitive: false}
-	has_secure_password
-	validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+  has_many :following, 
+  through: :active_relationships, 
+  source: :followed
+
+  has_many :followers,
+  through: :passive_relationships,
+  source: :follower
+
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save :downcase_email
+  before_save :create_activation_digest
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_save { self.email = email.downcase }
+  validates :name, presence:  true, length:{maximum: 50}
+  validates :email, presence: true, length:{maximum: 255},
+  format:{ with: VALID_EMAIL_REGEX}, 
+  uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
 	# Return the hash digest of the given string
 	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? 
-				BCrypt::Engine::MIN_COST :
-				BCrypt::Engine.cost
-		BCrypt::Password.create(string, cost: cost)
+    BCrypt::Engine::MIN_COST :
+    BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
 
-	end
+  end
 
 	# Returns a random token
 	def User.new_token
@@ -31,33 +47,33 @@ class User < ActiveRecord::Base
 	end
 
     # Remember a user in the database for use in persistent session
-	def remember
-		self.remember_token = User.new_token
-		update_attribute(:remember_digest, User.digest(remember_token))
-	end
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
 
   # Return true if the given token matches the digest
   def authenticated?(attribute, token)
-   	digest = send("#{attribute}_digest")  
-  	return false if digest.nil?
-   	BCrypt::Password.new(digest).is_password?(token)
+    digest = send("#{attribute}_digest")  
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
  # Forget a user
-  def forget
-   	update_attribute(:remember_token, nil)
-  end
- 
+ def forget
+  update_attribute(:remember_token, nil)
+end
+
  # Activates an account.
-  def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
-  end
+ def activate
+  update_attribute(:activated,    true)
+  update_attribute(:activated_at, Time.zone.now)
+end
 
  # Sends activation email.
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
-  end
+ def send_activation_email
+  UserMailer.account_activation(self).deliver_now
+end
 
  # Set the passsword reset attributes.
  def create_reset_digest
@@ -69,7 +85,7 @@ class User < ActiveRecord::Base
  # Sends password reset email
  def send_password_reset_email
   UserMailer.password_reset(self).deliver_now
- end
+end
 
   # Returns true if a password reset has expired.
   def password_reset_expired?
@@ -80,8 +96,23 @@ class User < ActiveRecord::Base
   def feed
     Micropost.where("user_id = ?", id)
   end
+
+  # Follow a user
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollow a user
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user
+  def following?(other_user)
+    following.include?(other_user)
+  end
   
-    private
+  private
 
       # Converts email to all lower-case.
       def downcase_email
@@ -93,4 +124,4 @@ class User < ActiveRecord::Base
       	self.activation_token = User.new_token
       	self.activation_digest = User.digest(activation_token)
       end
-end
+    end
