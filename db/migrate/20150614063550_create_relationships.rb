dbc5d0b53452aa78551b84b71a5d26020fb15b96
Zhou You <yollyhideout@gmail.com>
2015-06-14 14:42:03 +0800

    basic relationship model

--- db/migrate/20150614063550_create_relationships.rb
+++ db/migrate/20150614063550_create_relationships.rb
@@ -1,14 +1,13 @@
 class CreateRelationships < ActiveRecord::Migration
   def change
     create_table :relationships do |t|
-      t.string :follower_id
-      t.string :integer
+      t.integer :follower_id
       t.integer :followed_id
 
       t.timestamps null: false
     end
+  end
     add_index :relationships, :follower_id
     add_index :relationships, :followed_id
     add_index :relationships, [:follower_id, :followed_id], unique: true
-  end
 end
