479937046daaf99752ff5ccad2ebbccba865db7a
Zhou You <yollyhideout@gmail.com>
2015-06-29 19:39:24 +0800

    Use SSL and the Puma webserver in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
+
   # 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
